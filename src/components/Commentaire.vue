e57284ea493cf714f25097a8ee8b75705c32c3a9
Yanis A <yaberkane.business@gmail.com>
2020-12-12 22:56:06 +0100

    fix h4 et text area

--- src/components/Commentaire.vue
+++ src/components/Commentaire.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="wrapper" style="width: 100vw; display: flex; justify-content: center; align-items: center;">
     <form style="max-width: 30vw;">
-      <H4> Ajoutez un commentaire</H4>
-      <Textarea name="" id="" cols="50" rows="10"></Textarea>
+      <h4> Ajoutez un commentaire</h4>
+      <textarea name="" id="" cols="50" rows="10"></textarea>
       <button style="width: 100%" type="submit">Envoyer</button>
     </form>
   </div>
